a21cd2b5df4cb908be4804c32e9766ab980cd379
= <wildman700@gmail.com>
2022-06-29 11:37:05 -0600

    start

--- Program.cs
+++ Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using DinoGame2.Game.Casting;
+using DinoGame2.Game.Directing;
+using DinoGame2.Game.Scripting;
+using DinoGame2.Game.Services;
+
+namespace DinoGame2
+{
+    /// <summary>
+    /// The program's entry point.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Starts the program using the given arguments.
+        /// </summary>
+        /// <param name="args">The given arguments.</param>
+        static void Main(string[] args)
+        {
+            //creating the cast
+            Cast cast = new Cast();
+            cast.AddActor("Dino", new Dino());
+            cast.AddActor("Enemy", new Enemy());
+            cast.AddActor("Goal", new Goal());
+            cast.AddActor("Score", new Score());
+
+            //create the services
+            KeyboardService keyboardService = new KeyboardService();
+            VideoService videoService = new VideoService(false);
+
+            //creating the script
+            Script script = new Script();
+            script.AddAction("input", new Control_actors_action(keyboardService));
+            script.AddAction("update", new Handle_collision());
+            script.AddAction("update", new Movement());
+            script.AddAction("output", new Draw_actors(videoService));
+
+            //start the game
+            Director director = new Director(videoService);
+            director.StartGame(cast, script);
+        }
+    }
+}
+
