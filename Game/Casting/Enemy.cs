17796de5b062539fb0b81837ca519f575a34f962
= <wildman700@gmail.com>
2022-06-30 20:35:58 -0600

    start

--- Game/Casting/Enemy.cs
+++ Game/Casting/Enemy.cs
@@ -4,20 +4,35 @@ namespace DinoGame2.Game.Casting
 {
     public class Enemy : Actor
     {
+        int direction; 
         public Enemy()
         {
             Random random = new Random();
-            int direction = random.Next(1, 3);
+            direction = random.Next(1, 3);
         }
 
         //program the enemies movements
-
-
         public override void MoveNext()
         {
-            //give the 
+
+            Point NewPosition;
+            Point velocity;
+            //give the object a direction based on the direction variable
+            //code for moving to the right
             if (direction == 1)
-            {} 
+            {
+                velocity = new Point (1,0);
+               
+            }
+            //code for moving to the left
+            else if (direction == 2)
+            {
+                velocity = new Point (-1,0);
+
+            }
+            Point position = new Point(this.GetPosition().GetX(), this.GetPosition().GetY());
+            NewPosition = new Point(position.GetX() + velocity.GetX(), position.GetY() + velocity.GetY());
+            this.SetPosition(NewPosition);
         }
 
 
